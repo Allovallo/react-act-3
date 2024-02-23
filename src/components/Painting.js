4c89237d575256b28b532a962e6e9bb7cda0c60d
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-02-23 19:45:53 +0200

    files updated

---
+++ src/components/Painting.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import defaultImage from './default.jpg';
+
+const Painting = ({
+  imageUrl = defaultImage,
+  title,
+  profileUrl,
+  author = 'не відомо',
+  price,
+  quantity,
+}) => {
+  return (
+    <div>
+      <img src={imageUrl} alt={title} />
+      <h2>{title}</h2>
+      <p>
+        Автор: <a href={profileUrl}>{author}</a>
+      </p>
+      <p>Ціна: {price} кредитів</p>
+      <p>Доступність: {quantity < 10 ? 'закінчюється' : 'є в наявності'} </p>
+      <button type="button">Додати до корзини</button>
+    </div>
+  );
+};
+
+Painting.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  profileUrl: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  quantity: PropTypes.number.isRequired,
+};
+
+export default Painting;
